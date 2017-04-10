993ec97ffca47427e401525dc4c550a1b4e3c732
LiangJinsi <993535736@qq.com>
2017-04-10 21:45:55 +0800

    修改了按键图标

--- app/src/main/java/com/example/bangbangmail/SendActivity.java
+++ app/src/main/java/com/example/bangbangmail/SendActivity.java
@@ -14,6 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 public class SendActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
